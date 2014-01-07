1574cd5becea892fc25c1b3436b1dabd3c76cce8
Adam Stokes <adam.stokes@ubuntu.com>
2014-01-06 22:40:28 -0500

    Initial
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Net::Salesforce - Blah blah blah
+
+# SYNOPSIS
+
+    use Net::Salesforce;
+
+# DESCRIPTION
+
+Net::Salesforce is
+
+# AUTHOR
+
+Adam Stokes <adamjs@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2014- Adam Stokes
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Net/Salesforce.pm
@@ -0,0 +1,39 @@
+package Net::Salesforce;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::Salesforce - Authentication against Salesforce OAuth 2 endpoints.
+
+=head1 SYNOPSIS
+
+  use Net::Salesforce;
+
+=head1 DESCRIPTION
+
+Net::Salesforce is an authentication module for Salesforce OAuth 2.
+
+=head1 AUTHOR
+
+Adam Stokes E<lt>adamjs@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Adam Stokes
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
